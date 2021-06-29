eaf009dcd6586fb750eb5da8a8dc92a6f9059e58
Sylvain Just <sy.just@i-sloth.com>
2021-06-29 12:14:30 +0200

    refactor(todo): optimisation array recursive merge

--- src/includes/alma-wc-functions.php
+++ src/includes/alma-wc-functions.php
@@ -39,6 +39,7 @@ function alma_wc_price_from_cents( $price ) {
  *
  * @return array
  * @throws RuntimeException Throws when argument count invalid or an argument is not an array.
+ * @TODO check if array_replace_recursive can do the stuff here ?
  */
 function alma_wc_array_merge_recursive() {
 	if ( func_num_args() < 2 ) {
