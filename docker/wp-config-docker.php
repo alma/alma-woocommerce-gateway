9bc4898139133cf3d055576848222dd288edf8d3
Sylvain Just <sylvain.just-ext@getalma.eu>
2022-03-15 16:15:21 +0100

    security: remove unused "eval CONFIG_EXTRA"

--- docker/wp-config-docker.php
+++ docker/wp-config-docker.php
@@ -121,10 +121,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARD
 }
 // (we include this by default because reverse proxying is extremely common in container environments)
 
-if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
-	eval($configExtra);
-}
-
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
