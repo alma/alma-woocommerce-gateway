81d0a2058a841c63270348dbbb282afef9df9040
Gilles Dumas <circusmind@gmail.com>
2022-03-29 16:46:57 +0200

    feat: use 'payment_upon_trigger_enabled'order meta on order to check if payment was created with trigger option

--- src/includes/class-alma-wc-payment-upon-trigger.php
+++ src/includes/class-alma-wc-payment-upon-trigger.php
@@ -53,7 +53,7 @@ class Alma_WC_Payment_Upon_Trigger {
 			return;
 		}
 
-		if ( 'yes' !== alma_wc_plugin()->settings->payment_upon_trigger_enabled ) {
+		if ( ! get_post_meta( $order_id, 'payment_upon_trigger_enabled' ) ) {
 			return;
 		}
 
