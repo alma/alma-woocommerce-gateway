effa906ab0626e0c2d124fe89ae108f4727d9f74
Gilles Dumas <circusmind@gmail.com>
2022-04-04 13:31:34 +0200

    wip: wp_cron

--- src/uninstall.php
+++ src/uninstall.php
@@ -10,6 +10,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 global $wpdb;
 
+
+error_log(' gil uninstall.php' );
+
 require_once dirname( __FILE__ ) . '/includes/class-alma-wc-settings.php';
 delete_option( Alma_WC_Settings::OPTIONS_KEY );
 delete_option( 'alma_version' );
@@ -17,3 +20,20 @@ delete_option( 'alma_warnings_handled' );
 delete_option( 'alma_bootstrap_warning_message_dismissed' );
 delete_option( 'alma_bootstrap_warning_message' );
 $wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $wpdb->esc_like( 'alma_migration_' ) . '%' ) );
+
+$timestamp = wp_next_scheduled( 'alma_cron_hook_test' );
+wp_unschedule_event( $timestamp, 'alma_cron_hook_test' );
+
+
+error_log(' gil uninstall.php 2' );
+
+//register_deactivation_hook( __FILE__, 'bl_deactivate' );
+//
+//function bl_deactivate() {
+//	$timestamp = wp_next_scheduled( 'bl_cron_hook' );
+//	wp_unschedule_event( $timestamp, 'bl_cron_hook' );
+//}
+
+
+
+
