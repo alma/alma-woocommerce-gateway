4d48f6c49a35ddb350c8d79508981b506b9dd119
Gilles Dumas <circusmind@gmail.com>
2022-04-19 17:14:08 +0200

    wip: upgrading this script to retrieve css and fonts

--- bin/retrieve-widget-js-file.sh
+++ bin/retrieve-widget-js-file.sh
@@ -1,17 +1,43 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Author : Gilles Dumas
 # Date : 20220307
-# This script retrieves the js file to include in web pages who display the alma widget.
-# This js file is supposed to be unminified.
-# So the url below will need to be changed as soon as the unminified file will be reachable.
+# This script retrieves
+# -- the JS unminified file to include in web pages who display the alma widget.
+# -- the CSS file.
+# -- the fonts.  (todo not sure)
 
-file_url='https://cdn.jsdelivr.net/npm/@alma/widgets@2.x/dist/raw/widgets.umd.js';
-file_name='widgets.umd.js';
+files_url='https://cdn.jsdelivr.net/npm/@alma/widgets@2.x/dist/';
+raw_url=${files_url}'raw/';
 
-# the directory_prefix is the relative folder path where js resources are called in the plugin via the wp_enqueue_script() function.
-directory_prefix='../src/assets/js/';
+# the directory_prefix is the relative folder path where assets are called in the plugin via the wp_enqueue_script() function.
+assets_directory_prefix='../src/assets/';
 
-wget -vdc ${file_url};
-mv ${file_name} ${directory_prefix}
+js_file_name='widgets.umd.js';
+js_file_url=${raw_url}${js_file_name};
+
+css_file_name='widgets.min.css';
+css_file_url=${raw_url}${css_file_name};
+
+font_files_base_url=${files_url}'assets/fonts/';
+
+js_directory_prefix=${assets_directory_prefix}'js/';
+css_directory_prefix=${assets_directory_prefix}'css/';
+fonts_directory_prefix=${css_directory_prefix}'assets/fonts/';
+
+# retrieve fonts.
+declare -a font_files_array
+font_files_array=(Eina04-Bold.ttf Eina04-Bold.woff PublicSans-VariableFont_wght.ttf);
+for font_file in "${font_files_array[@]}"; do
+  wget -dc ${font_files_base_url}${font_file};
+  mv "${font_file}" ${fonts_directory_prefix}
+done;
+
+# retrieve JS.
+wget -dc ${js_file_url};
+mv ${js_file_name} ${js_directory_prefix}
+
+# retrieve CSS.
+wget -dc ${css_file_url};
+mv ${css_file_name} ${css_directory_prefix}
 
