0ea069a843c05c3ab097c28ea2eb70b92e8c21a0
Gilles Dumas <circusmind@gmail.com>
2022-04-26 11:55:02 +0200

    chore: store all assets for widget in a 'widget' folder

--- bin/retrieve-widget-files.sh
+++ bin/retrieve-widget-files.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 # Author : Gilles Dumas
-# Date : 20220307
+# Date : 20220426
 # This script retrieves
-# -- the JS unminified file to include in web pages who display the alma widget.
+# -- the JS unminified file to include in web pages to display the Alma widget.
 # -- the CSS file.
-# -- the fonts.  (todo not sure)
+# -- the fonts.
 
 files_url='https://cdn.jsdelivr.net/npm/@alma/widgets@2.x/dist/';
 raw_url=${files_url}'raw/';
 
 # the directory_prefix is the relative folder path where assets are called in the plugin via the wp_enqueue_script() function.
-assets_directory_prefix='../src/assets/';
+widget_directory_prefix='../src/assets/widget/';
+js_directory_prefix=${widget_directory_prefix}'js/';
+css_directory_prefix=${widget_directory_prefix}'css/';
+assets_directory_prefix=${css_directory_prefix}'assets/';
+fonts_directory_prefix=${assets_directory_prefix}'fonts/';
+
+for directory in $widget_directory_prefix $js_directory_prefix $css_directory_prefix $assets_directory_prefix $fonts_directory_prefix
+do
+  if [[ -d "$directory" ]] ; then
+    echo "$directory already exists on your filesystem. (but this is not a problem)"
+  else
+    mkdir $directory;
+  fi
+done
 
 js_file_name='widgets.umd.js';
 js_file_url=${raw_url}${js_file_name};
@@ -21,11 +34,7 @@ css_file_url=${raw_url}${css_file_name};
 
 font_files_base_url=${files_url}'assets/fonts/';
 
-js_directory_prefix=${assets_directory_prefix}'js/';
-css_directory_prefix=${assets_directory_prefix}'css/';
-fonts_directory_prefix=${css_directory_prefix}'assets/fonts/';
-
-# retrieve fonts.
+# --1-- retrieve fonts.
 declare -a font_files_array
 font_files_array=(Eina04-Bold.ttf Eina04-Bold.woff PublicSans-VariableFont_wght.ttf);
 for font_file in "${font_files_array[@]}"; do
@@ -33,11 +42,12 @@ for font_file in "${font_files_array[@]}"; do
   mv "${font_file}" ${fonts_directory_prefix}
 done;
 
-# retrieve JS.
+# --2-- retrieve JS.
 wget -dc ${js_file_url};
 mv ${js_file_name} ${js_directory_prefix}
 
-# retrieve CSS.
+# --3-- retrieve CSS.
 wget -dc ${css_file_url};
 mv ${css_file_name} ${css_directory_prefix}
 
+exit;
--- src/includes/class-alma-wc-generic-handler.php
+++ src/includes/class-alma-wc-generic-handler.php
@@ -131,10 +131,10 @@ class Alma_WC_Generic_Handler {
 		);
 
 		// Inject JS/CSS required for the eligibility/payment plans info display.
-		$alma_widgets_js_url = alma_wc_plugin()->get_asset_url( 'js/widgets.umd.js' );
+		$alma_widgets_js_url = alma_wc_plugin()->get_asset_url( 'widget/js/widgets.umd.js' );
 		wp_enqueue_script( 'alma-widgets', $alma_widgets_js_url, array(), ALMA_WC_VERSION, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 
-		$alma_widgets_css_url = alma_wc_plugin()->get_asset_url( 'css/widgets.min.css' );
+		$alma_widgets_css_url = alma_wc_plugin()->get_asset_url( 'widget/css/widgets.min.css' );
 		wp_enqueue_style( 'alma-widgets', $alma_widgets_css_url, array(), ALMA_WC_VERSION ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 
 		$alma_widgets_injection_url = alma_wc_plugin()->get_asset_url( 'js/alma-widgets-inject.js' );
