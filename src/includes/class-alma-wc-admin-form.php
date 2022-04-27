54cf33e1a7bc0e63b9baa3f7d895a3bb55be62cd
Sylvain Just <sylvain.just-ext@getalma.eu>
2022-04-27 15:48:34 +0200

    fix: add the url for alma documentation

--- src/includes/class-alma-wc-admin-form.php
+++ src/includes/class-alma-wc-admin-form.php
@@ -250,8 +250,8 @@ class Alma_WC_Admin_Form {
 				'title'       => __( 'Custom check variations event', 'alma-woocommerce-gateway' ),
 				'type'        => 'text',
 				'default'     => $default_settings['variable_product_check_variations_event'],
-				// translators: %s is a technical information.
-				'description' => sprintf( __( 'This is the javascript event triggered to refresh the widget on a variable product page when the customer changes the product variation. Default value is <strong>%s</strong>.<br />More technical informations on <a href="https://docs.getalma.eu/docs/woocommerce-installation-guide" target="_blank">Alma documentation</a>', 'alma-woocommerce-gateway' ), Alma_WC_Settings::DEFAULT_CHECK_VARIATIONS_EVENT ),
+				// translators: %1$s is technical information, %2$s is FAQ doc URL.
+				'description' => sprintf( __( 'This is the javascript event triggered on variables products page, when the customer change the product variation. Default value is <strong>%1$s</strong>.<br />More technical informations on <a href="%2$s" target="_blank">Alma documentation</a>', 'alma-woocommerce-gateway' ), Alma_WC_Settings::DEFAULT_CHECK_VARIATIONS_EVENT, 'https://docs.getalma.eu/docs/woocommerce-faq' ),
 			),
 			'variable_product_price_query_selector'   => array(
 				'title'       => __( 'Variable products price query selector', 'alma-woocommerce-gateway' ),
