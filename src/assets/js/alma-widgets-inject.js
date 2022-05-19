cfada45887da14753933e7b0390d794222975a02
Gilles Dumas <circusmind@gmail.com>
2022-05-19 11:12:50 +0200

    wip: refactoring js widget inject file

--- src/assets/js/alma-widgets-inject.js
+++ src/assets/js/alma-widgets-inject.js
@@ -8,121 +8,130 @@
  * Alma admin General Helpers.
  */
 function AlmaWidgetHelper() {
-	return {
-		/**
-		 * Check if widget is visible.
-		 *
-		 * @param elem
-		 * @returns {boolean}
-		 */
-		isVisible: function( elem ) {
-			return ! ! ( elem && ( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ) );
-		},
-		/**
-		 * Get amount Element.
-		 *
-		 * @returns {null|*}
-		 */
-		getAmountElement: function() {
-			var paymentPlansContainerId = "#alma-payment-plans";
-			var settings                = jQuery( paymentPlansContainerId ).data( 'settings' );
-			var amountQuerySelector     = settings.amountQuerySelector;
-			if ( amountQuerySelector ) {
-				return document.querySelector( amountQuerySelector );
-			}
+
+	/**
+	 * Check if widget is visible.
+	 *
+	 * @param elem
+	 * @returns {boolean}
+	 */
+	var isVisible = function( elem ) {
+		return ! ! ( elem && ( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ) );
+	}
+
+	/**
+	 * Get amount Element.
+	 *
+	 * @returns {null|*}
+	 */
+	var getAmountElement = function() {
+		var paymentPlansContainerId = getPaymentPlansContainerId();
+		var settings                = jQuery( paymentPlansContainerId ).data( 'settings' );
+		var amountQuerySelector     = settings.amountQuerySelector;
+		if ( amountQuerySelector ) {
+			return document.querySelector( amountQuerySelector );
+		}
+		return null;
+	}
+
+	/**
+	 * Get PaymentPlansContainerId.
+	 *
+	 * @returns String
+	 */
+	var getPaymentPlansContainerId = function() {
+		return '#alma-payment-plans';
+	}
+
+	/**
+	 * Get settings.
+	 *
+	 * @returns {null|*}
+	 */
+	var getSettings = function() {
+		var paymentPlansContainerId = getPaymentPlansContainerId();
+		return jQuery( paymentPlansContainerId ).data( 'settings' );
+	}
+
+	/**
+	 * AlmaInitWidget.
+	 *
+	 * @returns Null
+	 */
+	var AlmaInitWidget = function () {
+		// Make sure settings are up-to-date after a potential cart_totals refresh.
+		var settings = getSettings();
+		if ( ! settings ) {
+			return;
+		}
+
+		if (settings.hasExcludedProducts) {
 			return null;
-		},
-		/**
-		 * Get PaymentPlansContainerId.
-		 *
-		 * @returns String
-		 */
-		getPaymentPlansContainerId: function() {
-			return '#alma-payment-plans';
-		},
-		/**
-		 * Get settings.
-		 *
-		 * @returns {null|*}
-		 */
-		getSettings: function() {
-			var paymentPlansContainerId = "#alma-payment-plans";
-			return jQuery( paymentPlansContainerId ).data( 'settings' );
-		},
-		/**
-		 * AlmaInitWidget.
-		 *
-		 * @returns Null
-		 */
-		AlmaInitWidget: function () {
-			// Make sure settings are up-to-date after a potential cart_totals refresh.
-			var settings = AlmaWidgetHelper().getSettings();
-			if ( ! settings ) {
-				return null;
-			}
+		}
 
-			if (settings.hasExcludedProducts) {
-				return null;
-			}
+		var merchantId = settings.merchantId;
+		var amount     = parseInt( settings.amount );
 
-			var merchantId = settings.merchantId;
-			var amount     = parseInt( settings.amount );
+		var amountElement = getAmountElement();
+		if (amountElement) {
+			if (isVisible( amountElement )) {
+				var child = amountElement.firstChild;
+				while (child) {
+					if (child.nodeType === ( Node.TEXT_NODE || 3 )) {
+						var strAmount = child.data
+							.replace( settings.thousandSeparator, '' )
+							.replace( settings.decimalSeparator, '.' )
+							.replace( /[^\d.]/g, '' )
 
-			var amountElement = AlmaWidgetHelper().getAmountElement();
-			if (amountElement) {
-				if (AlmaWidgetHelper().isVisible( amountElement )) {
-					var child = amountElement.firstChild;
-					while (child) {
-						if (child.nodeType === ( Node.TEXT_NODE || 3 )) {
-							var strAmount = child.data
-								.replace( settings.thousandSeparator, '' )
-								.replace( settings.decimalSeparator, '.' )
-								.replace( /[^\d.]/g, '' )
-
-							amount = Alma.Utils.priceToCents( parseFloat( strAmount ) );
-							break;
-						}
-						child = child.nextSibling;
+						amount = Alma.Utils.priceToCents( parseFloat( strAmount ) );
+						break;
 					}
-				} else {
-					amount = 0
+					child = child.nextSibling;
 				}
 			}
+		}
 
-			var almaApiMode = Alma.ApiMode.TEST;
-			if (settings.apiMode === 'live') {
-				almaApiMode = Alma.ApiMode.LIVE;
-			}
-			var widgets = Alma.Widgets.initialize( merchantId, almaApiMode )
-			widgets.add(
-				Alma.Widgets.PaymentPlans,
-				{
-					container: AlmaWidgetHelper().getPaymentPlansContainerId(),
-					purchaseAmount: amount,
-					locale: settings.locale, // [optional, default: en]
-					hideIfNotEligible: false,
-					plans: settings.enabledPlans.map(
-						function ( plan ) {
-							return {
-								installmentsCount: plan.installments_count,
-								minAmount: plan.min_amount,
-								maxAmount: plan.max_amount,
-								deferredDays: plan.deferred_days,
-								deferredMonths: plan.deferred_months
-							}
+		var almaApiMode = Alma.ApiMode.TEST;
+		if (settings.apiMode === 'live') {
+			almaApiMode = Alma.ApiMode.LIVE;
+		}
+		var widgets = Alma.Widgets.initialize( merchantId, almaApiMode )
+		widgets.add(
+			Alma.Widgets.PaymentPlans,
+			{
+				container: getPaymentPlansContainerId(),
+				purchaseAmount: amount,
+				locale: settings.locale, // [optional, default: en]
+				hideIfNotEligible: false,
+				plans: settings.enabledPlans.map(
+					function ( plan ) {
+						return {
+							installmentsCount: plan.installments_count,
+							minAmount: plan.min_amount,
+							maxAmount: plan.max_amount,
+							deferredDays: plan.deferred_days,
+							deferredMonths: plan.deferred_months
 						}
-					)
-				}
-			)
-		},
+					}
+				)
+			}
+		)
+	}
+
+	return {
+		isVisible: isVisible,
+		getAmountElement: getAmountElement,
+		getPaymentPlansContainerId: getPaymentPlansContainerId,
+		getSettings: getSettings,
+		AlmaInitWidget: AlmaInitWidget
 	}
 }
 
-
 jQuery( document ).ready(
-	function ($) {
+	function ( $ ) {
 
-		var settings = AlmaWidgetHelper().getSettings();
+		var almaWidgetHelper = new AlmaWidgetHelper();
+		var settings         = almaWidgetHelper.getSettings();
 		if ( ! settings ) {
 			return;
 		}
@@ -131,7 +140,7 @@ jQuery( document ).ready(
 		var firstRender       = settings.firstRender;
 
 		if ( firstRender ) {
-			AlmaWidgetHelper().AlmaInitWidget();
+			almaWidgetHelper.AlmaInitWidget();
 		}
 
 		if ( jqueryUpdateEvent ) {
@@ -142,17 +151,17 @@ jQuery( document ).ready(
 					// or its disappearing when some choices are missing. We first try to find an ongoing animation to
 					// update our widget *after* the animation has taken place, so that it uses up-to-date information/DOM
 					// in AlmaInitWidget.
-					var amountElement = AlmaWidgetHelper().getAmountElement()
+					var amountElement = almaWidgetHelper.getAmountElement()
 					var timer         = $.timers.find(
-						function (t) {
-							return t.elem === jQuery( amountElement ).closest( '.woocommerce-variation' ).get( 0 )
+						function ( t ) {
+							return t.elem === jQuery( amountElement ).closest( '.woocommerce-variation' ).get( 0 );
 						}
 					)
 
 					if (timer) {
 						window.setTimeout( window.AlmaInitWidget, timer.anim.duration )
-					} else if (AlmaWidgetHelper().isVisible( amountElement ) || ! settings.amountQuerySelector) {
-						AlmaWidgetHelper().AlmaInitWidget()
+					} else if ( almaWidgetHelper.isVisible( amountElement ) || ! settings.amountQuerySelector ) {
+						almaWidgetHelper.AlmaInitWidget();
 					}
 				}
 			);
