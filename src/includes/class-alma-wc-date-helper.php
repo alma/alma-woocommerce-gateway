8668dd2d5d2a2640ef13fa92a7d26bba232d9f7e
Gilles Dumas <circusmind@gmail.com>
2022-06-15 13:05:12 +0200

    refactor: removing error_log() everywhere

--- src/includes/class-alma-wc-date-helper.php
+++ src/includes/class-alma-wc-date-helper.php
@@ -41,26 +41,12 @@ class Alma_WC_Date_Helper {
 			$to = strtotime( 'now' );
 		}
 
-		error_log( '$share_of_checkout_enabled_date = ' . $share_of_checkout_enabled_date );
-
-		error_log( '$from = ' . $from );
-		error_log( '$from = ' . gmdate( 'Y-m-d', $from ) );
-
-		error_log( '$to = ' . $to );
-		error_log( '$to = ' . gmdate( 'Y-m-d', $to ) );
-
 		$dates_in_interval = array();
 		$start_timestamp   = strtotime( 'now', $from );
-
-		// $start_timestamp   = strtotime( '+1 day', $from );
-
-		error_log( '$start_timestamp = ' . $start_timestamp );
-		error_log( '$start_timestamp = ' . gmdate( 'Y-m-d', $start_timestamp ) );
 
 		for ( $i = $start_timestamp; $i <= $to; $i = strtotime( '+1 day', $i ) ) {
 			if ( $i >= strtotime( $share_of_checkout_enabled_date ) ) {
 				$dates_in_interval[] = gmdate( 'Y-m-d', $i );
-				error_log( 'date dans la boucle = ' . gmdate( 'Y-m-d', $i ) );
 			}
 		}
 		return $dates_in_interval;
