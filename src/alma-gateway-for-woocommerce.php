5aeccd593d32d8fa7ffe9d60139e29e31c1b028a
Gilles Dumas <circusmind@gmail.com>
2022-06-21 09:15:49 +0200

    refactor: removing debug code

--- src/alma-gateway-for-woocommerce.php
+++ src/alma-gateway-for-woocommerce.php
@@ -74,32 +74,3 @@ function almapay_wc_plugin() {
 }
 
 almapay_wc_plugin()->try_running();
-
-// $debug_tags = array();
-// add_action( 'all', function ( $tag ) {
-// global $debug_tags;
-// if ( in_array( $tag, $debug_tags ) ) {
-// return;
-// }
-// error_log( '------> action = ' . $tag );
-// echo "<pre>" . $tag . "</pre>";
-// $debug_tags[] = $tag;
-// } );
-//
-
-function generateCallTrace() {
-	$e     = new Exception();
-	$trace = explode( "\n", $e->getTraceAsString() );
-	// reverse array to make steps line up chronologically
-	$trace = array_reverse( $trace );
-	array_shift( $trace ); // remove {main}
-	array_pop( $trace ); // remove call to this method
-	$length = count( $trace );
-	$result = array();
-
-	for ( $i = 0; $i < $length; $i++ ) {
-		$result[] = ( $i + 1 ) . ')' . substr( $trace[ $i ], strpos( $trace[ $i ], ' ' ) ); // replace '#someNum' with '$i)', set the right ordering
-	}
-
-	return "\t" . implode( "\n\t", $result );
-}
