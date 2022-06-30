2732e45ccd48c96cc8262720a1dff86f28f074b7
Gilles Dumas <circusmind@gmail.com>
2022-06-30 16:45:14 +0200

    refactor: sonarcloud compliance

--- src/includes/class-alma-wc-payment-gateway.php
+++ src/includes/class-alma-wc-payment-gateway.php
@@ -173,9 +173,9 @@ class Alma_WC_Payment_Gateway extends WC_Payment_Gateway {
 	public function is_available() {
 
 		$current_user = wp_get_current_user();
-        if ( ! in_array('administrator', $current_user->roles ) && 'live' !== alma_wc_plugin()->settings->environment ) {
-	        return false;
-        }
+		if ( ! in_array( 'administrator', $current_user->roles, true ) && 'live' !== alma_wc_plugin()->settings->environment ) {
+			return false;
+		}
 
 		if ( wc()->cart === null ) {
 			return parent::is_available();
