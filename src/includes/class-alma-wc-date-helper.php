0d51ff7d509175a5d20ad4c54423f3230caeccd4
Sylvain Just <sylvain.just-ext@getalma.eu>
2022-07-04 15:18:39 +0200

    refactor: code review in progress

--- src/includes/class-alma-wc-date-helper.php
+++ src/includes/class-alma-wc-date-helper.php
@@ -29,19 +29,14 @@ class Alma_WC_Date_Helper {
 	}
 
 	/**
-	 * Gets dates in an interval.
+	 * Gets dates in an interval until yesterday.
 	 *
-	 * @param string $share_of_checkout_enabled_date The share of checkout enabled date by the merchant.
-	 * @param string $last_update_date The last update date in timestamp format.
+	 * @param string $start_date The share of checkout enabled date by the merchant formatted as yyyy-mm-dd.
+	 *
 	 * @return array
 	 */
-	public function get_dates_in_interval( $share_of_checkout_enabled_date, $last_update_date ) {
-		$start_date      = $last_update_date;
-		$start_timestamp = $this->generate_timestamp( $start_date );
-		if ( $last_update_date < $share_of_checkout_enabled_date ) {
-			$start_date      = $share_of_checkout_enabled_date;
-			$start_timestamp = $this->generate_timestamp( $start_date );
-		}
+	public function get_dates_in_interval( $start_date ) {
+		$start_timestamp = strtotime( $start_date );
 
 		$dates_in_interval = array();
 		for ( $i = $start_timestamp; time() - $i > 86400; $i = strtotime( '+1 day', $i ) ) {
@@ -49,20 +44,6 @@ class Alma_WC_Date_Helper {
 		}
 
 		return $dates_in_interval;
-	}
-
-
-	/**
-	 * Generate a timestamp from a date formatted as "yyyy-mm-dd".
-	 *
-	 * @param string $date_yyyy_mm_dd A date.
-	 * @return false|int
-	 */
-	private function generate_timestamp( $date_yyyy_mm_dd ) {
-		$year  = substr( $date_yyyy_mm_dd, 0, 4 );
-		$month = substr( $date_yyyy_mm_dd, 5, 2 );
-		$day   = substr( $date_yyyy_mm_dd, 8, 2 );
-		return mktime( 0, 0, 0, $month, $day, $year );
 	}
 
 }
