a877459d5b22e88560062accf5d8f66ad67cccab
Gilles Dumas <circusmind@gmail.com>
2022-07-20 14:56:26 +0200

    refactor: testing is we are on a BO page of an order paid by Alma

--- src/includes/class-alma-wc-refund.php
+++ src/includes/class-alma-wc-refund.php
@@ -64,10 +64,15 @@ class Alma_WC_Refund {
 	 * @return void
 	 */
 	public function admin_init() {
+		if ( isset( $_GET['post'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+			$maybe_wc_order = wc_get_order( intval( $_GET['post'] ) ); // phpcs:ignore WordPress.Security.NonceVerification
+			if ( $maybe_wc_order instanceof WC_order && $this->helper->is_paid_with_alma( $maybe_wc_order ) ) {
+				add_action( 'admin_notices', array( $this, 'admin_notices' ), 10 );
+				add_action( 'woocommerce_order_item_add_action_buttons', array( $this, 'woocommerce_order_item_add_action_buttons' ), 10 );
+			}
+		}
 		add_action( 'woocommerce_order_partially_refunded', array( $this, 'woocommerce_order_partially_refunded' ), 10, 2 );
 		add_action( 'woocommerce_order_fully_refunded', array( $this, 'woocommerce_order_fully_refunded' ), 10, 2 );
-		add_action( 'admin_notices', array( $this, 'admin_notices' ), 10 );
-		add_action( 'woocommerce_order_item_add_action_buttons', array( $this, 'woocommerce_order_item_add_action_buttons' ), 10 );
 		add_filter( 'woocommerce_new_order_note_data', array( $this, 'woocommerce_new_order_note_data' ), 10, 1 );
 		add_action( 'woocommerce_order_status_changed', array( $this, 'woocommerce_order_status_changed' ), 10, 3 );
 	}
@@ -82,7 +87,6 @@ class Alma_WC_Refund {
 	 * @return void
 	 */
 	public function woocommerce_order_status_changed( $order_id, $previous_status, $next_status ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-
 		$order = wc_get_order( $order_id );
 		if (
 			'refunded' === $next_status &&
@@ -96,12 +100,13 @@ class Alma_WC_Refund {
 	/**
 	 * Filters WC order note for order fully refunded by order state changed to "refunded".
 	 *
+	 * @see https://woocommerce.github.io/code-reference/files/woocommerce-includes-class-wc-order.html#source-view.1724
 	 * @param array $comment_datas Information values about the order note.
 	 * @return array
 	 */
 	public function woocommerce_new_order_note_data( $comment_datas ) {
-
-		if ( 'Order status set to refunded. To return funds to the customer you will need to issue a refund through your payment gateway.' === $comment_datas['comment_content'] ) {
+		$wc_order = wc_get_order( $comment_datas['comment_post_ID'] );
+		if ( $this->helper->is_paid_with_alma( $wc_order ) && 'Order status set to refunded. To return funds to the customer you will need to issue a refund through your payment gateway.' === $comment_datas['comment_content'] ) {
 			/* translators: %s is a username. */
 			$comment_datas['comment_content'] = sprintf( __( 'Order fully refunded via Alma by %s.', 'alma-gateway-for-woocommerce' ), wp_get_current_user()->display_name );
 		}
@@ -115,13 +120,7 @@ class Alma_WC_Refund {
 	 * @return void
 	 */
 	public function woocommerce_order_item_add_action_buttons() {
-		if ( is_object( get_current_screen() ) && 'shop_order' === get_current_screen()->id ) {
-			$order_id = intval( $_GET['post'] ); // phpcs:ignore WordPress.Security.NonceVerification
-			$order    = wc_get_order( $order_id );
-			if ( $this->helper->is_paid_with_alma( $order ) ) {
-				add_filter( 'gettext', array( $this, 'gettext' ), 10, 3 );
-			}
-		}
+		add_filter( 'gettext', array( $this, 'gettext' ), 10, 3 );
 	}
 
 	/**
@@ -133,7 +132,6 @@ class Alma_WC_Refund {
 	 * @return mixed|string
 	 */
 	public function gettext( $translation, $text, $domain ) {
-
 		if ( 'woocommerce' !== $domain || ! array_key_exists( $text, $this->admin_texts_to_change ) || ! isset( $_GET['post'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
 			return $translation;
 		}
@@ -158,7 +156,6 @@ class Alma_WC_Refund {
 	 * @return void
 	 */
 	public function admin_notices() {
-
 		if ( 'shop_order' !== get_current_screen()->id ) {
 			return;
 		}
