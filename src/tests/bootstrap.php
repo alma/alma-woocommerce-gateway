dc0a6da60c2acd6a288b6147e7a99a13486c01e9
Claire Bonachera <claire.bonachera@getalma.eu>
2022-09-07 15:34:08 +0200

    get rid of tests, need better implementations

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -4,8 +4,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../includes/alma-wc-functions.php';
-/** LEGAL CHECKOUT FEATURE
- * require __DIR__ . '/../admin/helpers/class-alma-wc-admin-helper-share-of-checkout.php';
- * require __DIR__ .'/../includes/class-alma-wc-logger.php';
- * require __DIR__ .'/../admin/helpers/class-alma-wc-admin-helper-order.php';
- */
