c67c970bcb1543f4f503bfa986010fd696a38826
Claire Bonachera <claire.bonachera@getalma.eu>
2022-12-20 14:29:56 +0100

    fix: remove uninstall cron

--- src/uninstall.php
+++ src/uninstall.php
@@ -17,6 +17,3 @@ delete_option( 'alma_warnings_handled' );
 delete_option( 'alma_bootstrap_warning_message_dismissed' );
 delete_option( 'alma_bootstrap_warning_message' );
 $wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $wpdb->esc_like( 'alma_migration_' ) . '%' ) );
-
-$alma_wc_timestamp = wp_next_scheduled( Alma_WC_Share_Of_Checkout::CRON_ACTION );
-wp_unschedule_event( $alma_wc_timestamp, Alma_WC_Share_Of_Checkout::CRON_ACTION );
