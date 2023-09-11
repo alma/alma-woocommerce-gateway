cf1bd4d751d01a2468ed026d12a3ee53de85a746
ClaireAlma <claire.bonachera@getalma.eu>
2023-09-11 11:42:16 +0200

    fix variable name

--- src/assets/js/alma-checkout-in-page.js
+++ src/assets/js/alma-checkout-in-page.js
@@ -9,7 +9,7 @@
  */
 
 (function ($) {
-	var src    = /^general_([0-9]{1,2})_[0-9]{1,2}_[0-9]{1,2}/g;
+	var regex  = /^general_([0-9]{1,2})_[0-9]{1,2}_[0-9]{1,2}/g;
 	var inPage = undefined;
 
 	$( 'body' ).on(
