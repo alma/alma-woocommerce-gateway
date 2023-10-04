910f9c2caa0834e5480f5ded4fc0c257320ad023
Rémi Zuffinetti <remi.zuffinetti@getalma.eu>
2023-10-04 14:26:11 +0200

    Logs

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -144,6 +144,7 @@ install_db() {
 	fi
 
 	# create database
+	echo "mysqladmin create $DB_NAME --user=$DB_USER --password=$DB_PASS$EXTRA"
 	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
