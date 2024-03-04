8ad3c9fb8f7a7ab49e9b1b6fcce265cf55d9a118
CamilleFljt <camille.flajollet@getalma.eu>
2024-03-04 09:33:55 +0100

    feat(blocks): improve css

--- src/build/alma-checkout-blocks.asset.php
+++ src/build/alma-checkout-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '5fd21c56797e5bc1383f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7dc683ab954885c40ad8');
