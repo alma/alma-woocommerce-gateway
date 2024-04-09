d9b8eff68e7c9fa7438b55be79edddb22d8e0078
CamilleFljt <camille.flajollet@getalma.eu>
2024-04-09 15:24:23 +0200

    fix buttons margin bottom

--- src/build/alma-checkout-blocks.asset.php
+++ src/build/alma-checkout-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '2cc8a13908c769b5b357');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => 'c931ecd04fdbceff224d');
