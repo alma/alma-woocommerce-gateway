f7e4868f20baddc87567139ada12b83e7e8736fc
Claire <claire.bonachera@getalma.eu>
2024-04-25 14:22:55 +0200

    build new assets

--- src/build/alma-checkout-blocks.asset.php
+++ src/build/alma-checkout-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => 'e6257f31299b83a5eb63');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '34f2e04d5d1de70bce2f');
