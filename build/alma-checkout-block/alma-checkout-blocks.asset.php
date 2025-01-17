41759f3942594781ab1928f3c5c04dc57f48c58d
Francois-Gomis <francois.gomis@getalma.eu>
2025-01-17 16:51:41 +0100

    fix: update dependency

--- build/alma-checkout-block/alma-checkout-blocks.asset.php
+++ build/alma-checkout-block/alma-checkout-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '4cbab674743643262729');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element'), 'version' => '1cb9c3b3e948493b6c29');
