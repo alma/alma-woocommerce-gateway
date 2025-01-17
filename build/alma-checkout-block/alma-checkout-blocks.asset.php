e73d512108cc5b23effb807b7a468a3ef8851555
Francois-Gomis <francois.gomis@getalma.eu>
2025-01-17 10:39:28 +0100

    fix: build

--- build/alma-checkout-block/alma-checkout-blocks.asset.php
+++ build/alma-checkout-block/alma-checkout-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '446adef0675e6c9eb22c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '0e51981be653bc7d8e43');
