9a4e37c019659f4c2e406ba197e3dfcf3cded660
Martin Supiot <martin.supiot@getalma.eu>
2025-01-28 17:46:15 +0100

    fix: typo and lint

--- src/alma-checkout-block/components/Label.js
+++ src/alma-checkout-block/components/Label.js
@@ -1,14 +1,14 @@
 import {Logo} from "@alma/react-components";
 
-export const Label             = (props) => {
-	const {components, title}  = props;
-	const {PaymentMethodLabel} = components;
-	const icon                 = < Logo style = {{width: 'auto', height: '1em'}} logo = "alma-orange" / > ;
-	const text                 = < div > {title} < / div > ;
+export const Label = (props) => {
+    const {components, title} = props;
+    const {PaymentMethodLabel} = components;
+    const icon = <Logo style={{width: 'auto', height: '1em'}} logo="alma-orange"/>;
+    const text = <div> {title} </div>;
 
-	return (
-		< span className                      = "paymentMethodLabel" >
-					< PaymentMethodLabel text = {text} icon = {icon} / >
-				< / span >
-	);
+    return (
+        <span className="paymentMethodLabel">
+            <PaymentMethodLabel text={text} icon={icon}/>
+        </span>
+    );
 };
