d77808d94c984fb3de5045438e6bdd68625c386b
Martin Supiot <martin.supiot@getalma.eu>
2025-02-14 09:23:48 +0100

    fix: change phpcs configuration

--- bin/coding-standard-fixer.sh
+++ bin/coding-standard-fixer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-docker run --rm -v "$PROJECT_ROOT:/app" -w /app phpcbf --standard=phpcs.xml ./
+docker run --rm -v "$PROJECT_ROOT:/app" -w /app phpcbf:wp --standard=phpcs.xml ./
 EXIT_CODE=$?
 if [[ $EXIT_CODE -ne 0 ]]; then
     echo "Fix the errors with PHPcbf automatic fixer before commit!"
--- bin/coding-standard.sh
+++ bin/coding-standard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-docker run --rm -v "$PROJECT_ROOT:/app" -w /app phpcs --standard=phpcs.xml ./
+docker run --rm -v "$PROJECT_ROOT:/app" -w /app phpcs:wp --standard=phpcs.xml ./
 EXIT_CODE=$?
 if [[ $EXIT_CODE -ne 0 ]]; then
     echo "Fix the errors before commit!"
