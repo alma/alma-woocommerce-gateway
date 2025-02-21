89465b4a20df8bd0139e79b3f9e250d76a502026
Martin Supiot <martin.supiot@getalma.eu>
2025-02-21 18:15:07 +0100

    fix: clean linter

--- bin/lint-fix.sh
+++ bin/lint-fix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-docker run --rm -v "$PROJECT_ROOT:/app" -w /app lint:wc -v --standard=phpcs.xml ./
+docker run --rm -v "$(pwd):/app" -w /app lint:wc -v --standard=phpcs.xml ./
 EXIT_CODE=$?
 
 if [[ $EXIT_CODE -ne 0 ]]; then
