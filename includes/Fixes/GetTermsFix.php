16cf9dcaf447c8828e9282641092c46d4095187e
joyet simon <43644110+joyet-simon@users.noreply.github.com>
2025-03-10 11:33:22 +0100

    fix: get_terms function to be compatible with Wordpress 4.4

---
+++ includes/Fixes/GetTermsFix.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Get terms fix.
+ *
+ * @package Alma\Woocommerce\Fixes
+ */
+
+namespace Alma\Woocommerce\Fixes;
+
+/**
+ * Class GetTermsFix
+ * Fix for get_terms function to be compatible with WordPress 4.4.
+ */
+class GetTermsFix {
+
+	/**
+	 * Get terms.
+	 *
+	 * @param array $args Arguments.
+	 */
+	public static function get_terms( $args ) {
+		if ( defined( 'WP_VERSION' ) && version_compare( WP_VERSION, '4.5.0', '<' ) ) {
+			// phpcs:disable
+			get_terms(
+				$args['taxonomy'],
+				array_filter(
+					$args,
+					function ( $term ) {
+						return 'taxonomy' !== $term;
+					}
+				)
+			);
+			// phpcs:enable
+		} else {
+			get_terms( $args );
+		}
+	}
+}
