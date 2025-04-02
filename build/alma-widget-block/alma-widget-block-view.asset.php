9a6ebf62b642ce406b1bf3f67f62b1b876ea0b18
Benjamin Freoua <benjamin.freoua@getalma.eu>
2025-04-02 16:48:45 +0200

    fix: global style css in dashboard

--- build/alma-widget-block/alma-widget-block-view.asset.php
+++ build/alma-widget-block/alma-widget-block-view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-data', 'wp-element'), 'version' => '857422bd4eab743eb6eb');
+<?php return array('dependencies' => array('react', 'wp-data', 'wp-element'), 'version' => '184197a3f9c2befb3788');
