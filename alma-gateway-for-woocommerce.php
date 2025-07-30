16b0a0a32fe29402a34a1e5c61a2ce64ac22d1a0
Martin Supiot <martin@webaaz.com>
2025-07-30 15:39:51 +0200

    Revert "feat: update for wc 9.9.x/10.0.x compatibility"

--- alma-gateway-for-woocommerce.php
+++ alma-gateway-for-woocommerce.php
@@ -32,6 +32,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Alma Payment Gateway for WooCommerce. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
  */
+
 use Alma\Woocommerce\Blocks\AlmaWidgetBlock;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -59,6 +60,7 @@ if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins',
 
 require_once ALMA_PLUGIN_PATH . 'vendor/autoload.php';
 
+
 /**
  * Return instance of AlmaPlugin.
  *
@@ -68,13 +70,6 @@ require_once ALMA_PLUGIN_PATH . 'vendor/autoload.php';
 
 
 function alma_plugin() {
-
-	load_plugin_textdomain(
-		'alma-gateway-for-woocommerce',
-		false,
-		plugin_basename( ALMA_PLUGIN_PATH ) . '/languages'
-	);
-
 	static $plugin;
 
 	if ( ! isset( $plugin ) ) {
@@ -86,6 +81,18 @@ function alma_plugin() {
 
 /** Add the plugin. */
 add_action( 'plugins_loaded', 'alma_plugin' );
+
+/** Init the plugin */
+add_action(
+	'init',
+	function () {
+		load_plugin_textdomain(
+			'alma-gateway-for-woocommerce',
+			false,
+			plugin_basename( ALMA_PLUGIN_PATH ) . '/languages'
+		);
+	}
+);
 
 /**
  * Init custom_order_tables if available in Woocommerce version.
