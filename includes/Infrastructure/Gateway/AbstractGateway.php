50850144e39c6ff3725a1f80e54b61fa57c37bd5
Martin Supiot <martin.supiot@getalma.eu>
2025-12-08 10:40:06 +0100

    fix: duplicate event

--- includes/Infrastructure/Gateway/AbstractGateway.php
+++ includes/Infrastructure/Gateway/AbstractGateway.php
@@ -133,9 +133,9 @@ abstract class AbstractGateway extends WC_Payment_Gateway {
 		$result = array();
 		if ( $config_service->isInPageEnabled() ) {
 			// In-page checkout with fallback redirection
-			$result['payment_id'] = $payment->getId();
-			$result['result']     = 'success';
-			$result['redirect']   = InPageHelper::getInPageRedirectionFallbackUrl( $payment->getId() );
+			$result['alma_payment_id'] = $payment->getId();
+			$result['result']          = 'success';
+			$result['redirect']        = InPageHelper::getInPageRedirectionFallbackUrl( $payment->getId() );
 		} else {
 			// Classic checkout redirection
 			$result['result']   = 'success';
