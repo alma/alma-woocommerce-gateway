2279dfae277540c34f500c0ef7d48f2d41655be1
Martin Supiot <martin.supiot@almapay.com>
2026-02-05 15:11:19 +0100

    feat: improve description and titles migration (#500)

--- includes/Infrastructure/Service/MigrationService.php
+++ includes/Infrastructure/Service/MigrationService.php
@@ -2,6 +2,8 @@
 
 namespace Alma\Gateway\Infrastructure\Service;
 
+use Alma\Gateway\Infrastructure\Helper\ContextHelper;
+
 class MigrationService {
 
 	const VERSION_KEY = 'alma_version';
@@ -34,8 +36,10 @@ class MigrationService {
 				// Add a migration lock to prevent concurrent migrations
 				add_option( self::MIGRATION_LOCK, time() );
 
+				$isBlocks = ContextHelper::isCheckoutPageUseBlocks();
+
 				// Migration from version 5 to version 6
-				$migratedData = $this->migrateFromV5ToV6( get_option( self::OLD_KEY, [] ) );
+				$migratedData = $this->migrateFromV5ToV6( get_option( self::OLD_KEY, [] ), $isBlocks );
 
 				// If merchant_id is missing or empty, invalidate the API keys
 				if ( ! array_key_exists( 'merchant_id', $migratedData ) || empty( $migratedData['merchant_id'] ) ) {
@@ -62,14 +66,16 @@ class MigrationService {
 	 * Migrate data from version 5 to version 6.
 	 *
 	 * @param array $originData The original data from version 5.
+	 * @param bool  $isBlocks Whether the checkout blocks are enabled.
 	 *
 	 * @return array The migrated data for version 6.
 	 */
-	public function migrateFromV5ToV6( array $originData ): array {
+	public function migrateFromV5ToV6( array $originData, bool $isBlocks = true ): array {
+		$isInPage     = isset( $originData['display_in_page'] ) && $originData['display_in_page'] === 'yes';
 		$migratedData = array_merge(
 			$this->migrateBasicSettings( $originData ),
-			$this->migrateDescriptions( $originData ),
-			$this->migrateTitles( $originData ),
+			$this->migrateDescriptions( $originData, $isBlocks, $isInPage ),
+			$this->migrateTitles( $originData, $isBlocks, $isInPage ),
 			$this->migrateWidgetSettings( $originData ),
 			$this->migratePaymentPlans( $originData ),
 			$this->migrateAmountLimits( $originData )
@@ -90,11 +96,12 @@ class MigrationService {
 	 */
 	private function migrateBasicSettings( array $originData ): array {
 		return [
-			'debug'        => $originData['debug'] ?? 'no',
-			'enabled'      => $originData['enabled'] ?? 'no',
-			'live_api_key' => $originData['live_api_key'] ?? null,
-			'test_api_key' => $originData['test_api_key'] ?? null,
-			'environment'  => $originData['environment'] ?? 'test',
+			'debug'                     => $originData['debug'] ?? 'no',
+			'enabled'                   => $originData['enabled'] ?? 'no',
+			'live_api_key'              => $originData['live_api_key'] ?? null,
+			'test_api_key'              => $originData['test_api_key'] ?? null,
+			'environment'               => $originData['environment'] ?? 'test',
+			'excluded_products_message' => $originData['cart_not_eligible_message_gift_cards'] ?? null,
 		];
 	}
 
@@ -102,36 +109,21 @@ class MigrationService {
 	 * Migrate description fields.
 	 *
 	 * @param array $originData The original data from version 5.
+	 * @param bool  $isBlocks Whether the checkout blocks are enabled.
+	 * @param bool  $isInPage Whether the in-page widget is enabled.
 	 *
 	 * @return array The migrated descriptions.
 	 */
-	private function migrateDescriptions( array $originData ): array {
+	private function migrateDescriptions( array $originData, bool $isBlocks, bool $isInPage ): array {
 		return [
-			'excluded_products_message'  => $originData['cart_not_eligible_message_gift_cards'] ?? null,
-			'pnx_description_field'      => $this->getFirstAvailableValue( $originData, [
-				'description_alma',
-				'description_alma_in_page',
-				'description_blocks_alma',
-				'description_blocks_alma_in_page'
-			] ),
-			'paylater_description_field' => $this->getFirstAvailableValue( $originData, [
-				'description_alma_pay_later',
-				'description_alma_in_page_pay_later',
-				'description_blocks_alma_pay_later',
-				'description_blocks_alma_in_page_pay_later'
-			] ),
-			'paynow_description_field'   => $this->getFirstAvailableValue( $originData, [
-				'description_alma_pay_now',
-				'description_alma_in_page_pay_now',
-				'description_blocks_alma_pay_now',
-				'description_blocks_alma_in_page_pay_now'
-			] ),
-			'credit_description_field'   => $this->getFirstAvailableValue( $originData, [
-				'description_alma_pnx_plus_4',
-				'description_alma_in_page_pnx_plus_4',
-				'description_blocks_alma_pnx_plus_4',
-				'description_blocks_alma_in_page_pnx_plus_4'
-			] ),
+			'pnx_description_field'      => $originData[ $this->formatString( 'description', $isBlocks,
+					$isInPage ) ] ?? null,
+			'paylater_description_field' => $originData[ $this->formatString( 'description', $isBlocks, $isInPage,
+					'pay_later' ) ] ?? null,
+			'paynow_description_field'   => $originData[ $this->formatString( 'description', $isBlocks, $isInPage,
+					'pay_now' ) ] ?? null,
+			'credit_description_field'   => $originData[ $this->formatString( 'description', $isBlocks, $isInPage,
+					'pnx_plus_4' ) ] ?? null,
 		];
 	}
 
@@ -139,36 +131,51 @@ class MigrationService {
 	 * Migrate title fields.
 	 *
 	 * @param array $originData The original data from version 5.
+	 * @param bool  $isBlocks Whether the checkout blocks are enabled.
+	 * @param bool  $isInPage Whether the in-page widget is enabled.
 	 *
 	 * @return array The migrated titles.
 	 */
-	private function migrateTitles( array $originData ): array {
+	private function migrateTitles( array $originData, bool $isBlocks, bool $isInPage ): array {
 		return [
-			'pnx_title_field'      => $this->getFirstAvailableValue( $originData, [
-				'title_alma',
-				'title_alma_in_page',
-				'title_blocks_alma',
-				'title_blocks_alma_in_page'
-			] ),
-			'paylater_title_field' => $this->getFirstAvailableValue( $originData, [
-				'title_alma_pay_later',
-				'title_alma_in_page_pay_later',
-				'title_blocks_alma_pay_later',
-				'title_blocks_alma_in_page_pay_later'
-			] ),
-			'paynow_title_field'   => $this->getFirstAvailableValue( $originData, [
-				'title_alma_pay_now',
-				'title_alma_in_page_pay_now',
-				'title_blocks_alma_pay_now',
-				'title_blocks_alma_in_page_pay_now'
-			] ),
-			'credit_title_field'   => $this->getFirstAvailableValue( $originData, [
-				'title_alma_pnx_plus_4',
-				'title_alma_in_page_pnx_plus_4',
-				'title_blocks_alma_pnx_plus_4',
-				'title_blocks_alma_in_page_pnx_plus_4'
-			] ),
+			'pnx_title_field'      => $originData[ $this->formatString( 'title', $isBlocks, $isInPage ) ] ?? null,
+			'paylater_title_field' => $originData[ $this->formatString( 'title', $isBlocks, $isInPage,
+					'pay_later' ) ] ?? null,
+			'paynow_title_field'   => $originData[ $this->formatString( 'title', $isBlocks, $isInPage,
+					'pay_now' ) ] ?? null,
+			'credit_title_field'   => $originData[ $this->formatString( 'title', $isBlocks, $isInPage,
+					'pnx_plus_4' ) ] ?? null,
 		];
+	}
+
+	/**
+	 * Format the string based on the given parameters.
+	 *
+	 * @param string $base The base string to format.
+	 * @param bool   $isBlocks Whether the checkout blocks are enabled.
+	 * @param bool   $isInPage Whether the in-page widget is enabled.
+	 * @param string $suffix An optional suffix to append to the string.
+	 *
+	 * @return string
+	 */
+	private function formatString( string $base, bool $isBlocks, bool $isInPage, string $suffix = '' ): string {
+		$parts = [ $base ];
+
+		if ( $isBlocks ) {
+			$parts[] = 'blocks';
+		}
+
+		$parts[] = 'alma';
+
+		if ( $isInPage ) {
+			$parts[] = 'in_page';
+		}
+
+		if ( ! empty( $suffix ) ) {
+			$parts[] = $suffix;
+		}
+
+		return implode( '_', $parts );
 	}
 
 	/**
@@ -272,23 +279,5 @@ class MigrationService {
 		}
 
 		return empty( $termIds ) ? null : $termIds;
-	}
-
-	/**
-	 * Get the first available value from the data array based on the provided keys.
-	 *
-	 * @param array $data The data array to search.
-	 * @param array $keys The list of keys to check in order.
-	 *
-	 * @return ?string The first available value or null if none found.
-	 */
-	private function getFirstAvailableValue( array $data, array $keys ): ?string {
-		foreach ( $keys as $key ) {
-			if ( isset( $data[ $key ] ) ) {
-				return $data[ $key ];
-			}
-		}
-
-		return null;
 	}
 }
