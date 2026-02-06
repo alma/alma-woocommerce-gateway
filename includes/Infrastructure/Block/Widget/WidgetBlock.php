badb5556ec2ba7423c2cc4c206dbdec1d36ea88a
Martin Supiot <martin.supiot@getalma.eu>
2026-02-06 18:48:22 +0100

    fix: cart adpater was badly initialized

--- includes/Infrastructure/Block/Widget/WidgetBlock.php
+++ includes/Infrastructure/Block/Widget/WidgetBlock.php
@@ -13,14 +13,13 @@ namespace Alma\Gateway\Infrastructure\Block\Widget;
 
 use Alma\Gateway\Application\Helper\ExcludedProductsHelper;
 use Alma\Gateway\Application\Service\ConfigService;
+use Alma\Gateway\Infrastructure\Exception\Block\WidgetBlockException;
 use Alma\Gateway\Infrastructure\Adapter\CartAdapter;
-use Alma\Gateway\Infrastructure\Exception\Repository\FeePlanRepositoryException;
 use Alma\Gateway\Infrastructure\Helper\AssetsHelper;
 use Alma\Gateway\Infrastructure\Helper\ContextHelper;
 use Alma\Gateway\Infrastructure\Mapper\FeePlanListMapper;
 use Alma\Gateway\Infrastructure\Repository\FeePlanRepository;
 use Alma\Gateway\Infrastructure\Repository\GatewayRepository;
-use Alma\Gateway\Infrastructure\Service\AssetsService;
 use Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface;
 
 
@@ -36,9 +35,6 @@ class WidgetBlock implements IntegrationInterface {
 	/** @var ConfigService The settings. */
 	protected ConfigService $config_service;
 
-	/** @var CartAdapter The Cart. */
-	private CartAdapter $cart_adapter;
-
 	/** @var ContextHelper */
 	private ContextHelper $context_helper;
 
@@ -48,8 +44,6 @@ class WidgetBlock implements IntegrationInterface {
 	/** @var GatewayRepository The Gateway Repository */
 	private GatewayRepository $gateway_repository;
 
-	/** @var AssetsService */
-	private AssetsService $assets_service;
 	/** @var ExcludedProductsHelper */
 	private ExcludedProductsHelper $excluded_products_helper;
 
@@ -59,15 +53,12 @@ class WidgetBlock implements IntegrationInterface {
 		FeePlanRepository $fee_plan_repository,
 		GatewayRepository $gateway_repository,
 		ContextHelper $context_helper,
-		AssetsService $assets_service,
 		ExcludedProductsHelper $excluded_products_helper
 	) {
 		$this->config_service           = $config_service;
-		$this->cart_adapter             = $cart_adapter;
 		$this->fee_plan_repository      = $fee_plan_repository;
 		$this->gateway_repository       = $gateway_repository;
 		$this->context_helper           = $context_helper;
-		$this->assets_service           = $assets_service;
 		$this->excluded_products_helper = $excluded_products_helper;
 	}
 
@@ -91,14 +82,18 @@ class WidgetBlock implements IntegrationInterface {
 	 * Create new scratch file from selection
 	 * const settings = window.wc.wcSettings.getSetting(`alma-widget-block_data`, null);
 	 * @return array
-	 * @throws FeePlanRepositoryException
+	 *
+	 * @throws WidgetBlockException
 	 * @see src/alma-widget-block/AlmaWidget.js
 	 */
 	public function get_script_data(): array {
 
-		$excludedCategories     = $this->config_service->getExcludedCategories();
+		$excludedCategories = $this->config_service->getExcludedCategories();
+
+		$cart_adapter = ContextHelper::getCart();
+
 		$canDisplayWidgetOnCart = $this->excluded_products_helper->canDisplayOnCartPage(
-			$this->cart_adapter,
+			$cart_adapter,
 			$excludedCategories
 		);
 		$feePlanList            = $this->fee_plan_repository->getAllWithEligibility()->filterEnabled()->orderBy( $this->gateway_repository->findOrderedAlmaGateways() );
@@ -107,7 +102,7 @@ class WidgetBlock implements IntegrationInterface {
 			'merchant_id'                 => $this->config_service->getMerchantId(),
 			'environment'                 => strtoupper( $this->config_service->getEnvironment()->getMode() ),
 			'plans'                       => ( new FeePlanListMapper() )->buildFeePlanListDto( $feePlanList )->toArray()['plans'],
-			'amount'                      => $this->cart_adapter->getCartTotal(),
+			'amount'                      => $cart_adapter->getCartTotal(),
 			'locale'                      => $this->context_helper->getLanguage(),
 			'can_be_displayed'            => count( $feePlanList ) > 0 && $this->config_service->getWidgetCartEnabled(),
 			'is_excluded_categories'      => ! $canDisplayWidgetOnCart,
