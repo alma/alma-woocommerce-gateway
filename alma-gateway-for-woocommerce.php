d4c8362034107f2bd4c627157c840ade94d6488f
Martin Supiot <martin.supiot@getalma.eu>
2026-02-17 14:22:03 +0100

    feat: update translations

--- alma-gateway-for-woocommerce.php
+++ alma-gateway-for-woocommerce.php
@@ -12,6 +12,7 @@
  * Domain Path: /languages
  * Requires at least: 6.6.0
  * Requires PHP: 7.4
+ * Requires Plugins: woocommerce
  * Tested up to: 6.9.1
  *
  * @package Alma_Gateway_For_Woocommerce
